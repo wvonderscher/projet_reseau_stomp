df383f3622d1c46998adb14d9b165f706d89c2f3
William VONDERSCHER <wvonderscher@gmail.com>
2022-05-03 20:47:45 +0200

    ajout du squelette des premières fonctions

--- dist/server/server.js
+++ dist/server/server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const app = express();
+const connectionRegex = /^CONNECT/;
+//créer liste des subscribe
 //initialize a simple http server
 const server = http.createServer(app);
 //initialize the WebSocket server instance
@@ -13,13 +15,38 @@ wss.on('connection', (ws) => {
     ws.on('message', (message) => {
         //log the received message and send it back to the client
         console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+        //on vérifie si le client est bien connecté
+        typeRequete(splitRequete(message), ws);
+        // if (connectionRegex.test(message)) {
+        //     message = String(message).replace(connectionRegex, '');
+        //     //send back the message to the other clients
+        //     wss.clients
+        //         .forEach(client => {
+        //            //if (client != ws) {
+        //                 client.send(`Hello, broadcast message -> ${message}`);
+        //             //}    
+        //         });
+        // } else {
+        //     ws.send(`Hello, you sent -> ${message}`);
+        // }
     });
     //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
+    ws.send('connecté');
 });
 //start our server
 server.listen(process.env.PORT || 8999, () => {
     //console.log(`Server started on port ${server.address().port} :)`);
 });
+function typeRequete(requete, ws) {
+    switch (requete[0]) {
+        case "CONNECT":
+            ws.send("CA MARCHE");
+            break;
+        default:
+            ws.send("ERROR : " + requete[0]);
+    }
+}
+function splitRequete(msg) {
+    return String(msg).split(/\r?\n/);
+}
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,14 @@ import * as http from 'http';
 import * as WebSocket from 'ws';
 
 const app = express();
+const connectionRegex = /^CONNECT/;
+const host = "ws://localhost:8999/";
+const clientConnected = [];
+//créer liste des subscribe
 
 //initialize a simple http server
 const server = http.createServer(app);
+
 
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
@@ -15,16 +20,77 @@ wss.on('connection', (ws: WebSocket) => {
     //connection is up, let's add a simple simple event
     ws.on('message', (message: string) => {
 
+
         //log the received message and send it back to the client
         console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+
+        //On regarde la requete envoyé par le 
+        typeRequete(splitRequete(message), ws);
     });
 
     //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
+    ws.send('connecté');
 });
 
 //start our server
 server.listen(process.env.PORT || 8999, () => {
     //console.log(`Server started on port ${server.address().port} :)`);
 });
+
+//fonction qui va vérifier si la frame de la requete existe et traiter cette dernière.
+function typeRequete(requete: String[], ws: WebSocket){
+    
+    switch(requete[0]){
+        case "CONNECT":
+            //required: accept-version ,host
+
+
+        break;
+        case "SEND":
+            //required: destination
+        break;
+        case "SUBSCRIBE":
+            //required: destination, id
+        break;
+        case "UNSUBSCRIBE":
+            //required: id
+        break;
+        case "ACK":
+            //required: id
+        break;
+        case "DISCONNECT":
+            //required:
+        break;
+        default:
+            //si aucune des Frames est connue, on renvoie ERROR pour prévenir le client
+            ws.
+    }
+}
+
+
+//fonction qui permet de split une requete dans le but de traiter le différentes parties.
+function splitRequete(msg: string): String[]{
+    return String(msg).split(/\r?\n/);
+}
+
+//CE QUE LE SERVEUR RECOIT
+//"MESSAGE":
+    //required: destination, message-id, subscription
+
+//"RECEIPT":
+    //required: receipt-is
+
+    //fonction qui permet l'envoie à un client une frame ERROR
+function envoyerErreur(msgHeader: String, msgBody: String){
+    return "ERROR\nmessage:"+msgHeader+"\n"
+}
+
+function connecting(rqt: String[]){
+    //verification des headers
+    //si un header mal formé : return ERROR
+    //si tout bon : on ajoute le client dans la liste des clients connectes
+    let arg1= rqt[1].split(':');
+    if((arg1[0] === "accept-version") && (arg1[1] === "1.2" || arg1[1] === "1.1" || arg1[1] === "1.0") ){
+        
+    }
+}
